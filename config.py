cfa7a8f6c8ce122b306892b914d78b52e43cca4c
royr40122-oss <royr40122@gmail.com>
2026-02-28 14:56:19 +0530

    Update  config.py

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6933683980:AAFcoeJR272htDJu1Z1mDMdRAC5J0ylETzw")
-    API_ID = int(os.environ["API_ID", 29687224]
-    API_HASH = os.environ["API_HASH", "c543dbfe3007000fbecbd48c10731a55"]
-    AUTH_USERS = "6427042108"""
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7952763523:AAEDuVFshqXJL2vIicFbMYwqQzX12-TkdtE")
+    API_ID = int(os.environ["API_ID", 31445635]
+    API_HASH = os.environ["API_HASH", "15c057ca3aa85360ac3d63de52368718"]
+    AUTH_USERS = "8441048952"""
+
 
